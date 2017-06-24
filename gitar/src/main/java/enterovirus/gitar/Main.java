d7f5ce0675162f0e0c387ec62e9682df5f8f7046
Cong-Xin Qiu <ozoox.o@gmail.com>
2017-06-24 20:08:43 +0800

    Capsid refer to gitar

--- gitar/src/main/java/enterovirus/gitar/Main.java
+++ gitar/src/main/java/enterovirus/gitar/Main.java
@@ -1,6 +1,11 @@
 package enterovirus.gitar;
 
 public class Main {
+	
+	public static String hello () {
+		return "Hello enterovirus gitar!";
+	}
+	
 	public static void main(String[] args) {
 		System.out.println("Hello enterovirus gitar!");
 	}
