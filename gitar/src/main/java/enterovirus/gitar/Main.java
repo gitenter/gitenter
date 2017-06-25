7ba5286351755e70396f4125aa036119d9980d25
Cong-Xin Qiu <ozoox.o@gmail.com>
2017-06-25 16:33:25 +0800

    JGit first tryout

--- gitar/src/main/java/enterovirus/gitar/Main.java
+++ gitar/src/main/java/enterovirus/gitar/Main.java
@@ -1,12 +1,32 @@
 package enterovirus.gitar;
 
+import java.io.File;
+import java.io.IOException;
+
+import org.eclipse.jgit.lib.Ref;
+import org.eclipse.jgit.lib.Repository;
+import org.eclipse.jgit.storage.file.FileRepositoryBuilder;
+
 public class Main {
 	
 	public static String hello () {
 		return "Hello enterovirus gitar!";
 	}
 	
-	public static void main(String[] args) {
+	static void openGit (File file) throws IOException {
+
+        FileRepositoryBuilder builder = new FileRepositoryBuilder();
+		Repository repo = builder.setGitDir(file).readEnvironment().findGitDir().build();
+		System.out.println(repo.getDirectory().toString());
+
+		Ref master = repo.exactRef("refs/heads/master");
+		System.out.println(master.getObjectId().toString());
+	}
+	
+	public static void main(String[] args) throws IOException {
+		
+		openGit(new File("/home/beta/git/client_1/.git"));
+		
 		System.out.println("Hello enterovirus gitar!");
 	}
 }
