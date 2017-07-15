b5018957eb946414589674816f7832ffd1148109
Cong-Xin Qiu <ozoox.o@gmail.com>
2017-07-15 15:05:12 +0800

    Correct some sql typo

--- database/initiate_database.sql
+++ database/initiate_database.sql
@@ -1,20 +1,10 @@
 CREATE SCHEMA setting;
 
-CREATE TABLE setting.user_roll (
-	id serial PRIMARY KEY,
-	name text NOT NULL UNIQUE
-);
-
-INSERT INTO setting.repository_user_roll VALUES
-	(1, 'editor'),
-	(2, 'reviewer'),
-	(3, 'reader');
-
 --------------------------------------------------------------------------------
 
 CREATE SCHEMA config;
 
-CREATE TABLE config.user (
+CREATE TABLE config.person (
 	id serial PRIMARY KEY,
 	username text NOT NULL UNIQUE,
 	password text NOT NULL,
@@ -28,16 +18,16 @@ CREATE TABLE config.organization (
 	display_name text
 );
 
-CREATE TABLE config.organization_user_management_map (
-	organization_id serial REFERENCES config.user (id) ON DELETE CASCADE,
-	user_id serial REFERENCES config.user (id) ON DELETE RESTRICT,
-	PRIMARY KEY (organization_id, user_id)
+CREATE TABLE config.organization_manager_map (
+	organization_id serial REFERENCES config.organization (id) ON DELETE CASCADE,
+	person_id serial REFERENCES config.person (id) ON DELETE RESTRICT,
+	PRIMARY KEY (organization_id, person_id)
 );
 
 CREATE TABLE config.repository (
 	id serial PRIMARY KEY,
 
-	organization_id serial REFERENCES config.user (id) ON DELETE CASCADE,
+	organization_id serial REFERENCES config.organization (id) ON DELETE CASCADE,
 	name text NOT NULL,
 	display_name text,
 	UNIQUE (organization_id, name),
@@ -45,12 +35,22 @@ CREATE TABLE config.repository (
 	git_uri text NOT NULL
 );
 
-CREATE TABLE config.repository_user_map (
+CREATE TABLE setting.repository_person_roll (
+	id serial PRIMARY KEY,
+	name text NOT NULL UNIQUE
+);
+
+INSERT INTO setting.repository_person_roll VALUES
+	(1, 'editor'),
+	(2, 'reviewer'),
+	(3, 'reader');
+
+CREATE TABLE config.repository_person_map (
 	id serial PRIMARY KEY,
 	repository_id serial REFERENCES config.repository (id) ON DELETE CASCADE,
-	user_id serial REFERENCES config.user (id) ON DELETE CASCADE,
-	roll serial REFERENCES setting.repository_user_roll (id) ON DELETE RESTRICT,
-	UNIQUE (repository_id, user_id)
+	person_id serial REFERENCES config.person (id) ON DELETE CASCADE,
+	roll serial REFERENCES setting.repository_person_roll (id) ON DELETE RESTRICT,
+	UNIQUE (repository_id, person_id)
 );
 
 --------------------------------------------------------------------------------
@@ -60,5 +60,5 @@ CREATE SCHEMA git;
 CREATE TABLE git.commit (
 	id serial PRIMARY KEY,
 	repository_id serial REFERENCES config.repository (id) ON DELETE CASCADE,
-	sha text NOT NULL UNIQUE
+	sha_checksum_hash text NOT NULL UNIQUE
 );
