c7bd15dbc10892c7c2d63be772dbd0361434b2c4
Cong-Xin Qiu <ozoox.o@gmail.com>
2017-07-22 15:19:19 +0800

    Tiny comments of HAL API

--- capsid/src/main/java/enterovirus/capsid/database/OrganizationRepository.java
+++ capsid/src/main/java/enterovirus/capsid/database/OrganizationRepository.java
@@ -7,6 +7,11 @@ import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import enterovirus.capsid.domain.OrganizationBean;
 
+/*
+ * Notice that the HAL-formatted API also support PUT, PATCH and DELETE:
+ * 
+ * curl -X PUT -H "Content-Type:applicationjson" -d "{ \"name\": \"gov\", \"displayName\": \"govgov\" }" http://localhost:8888/hal/organizations/1
+ */
 @RepositoryRestResource(collectionResourceRel="organizations", path="organizations")
 public interface OrganizationRepository extends PagingAndSortingRepository<OrganizationBean, Integer> {
 
