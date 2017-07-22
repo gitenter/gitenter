0130772aad85920923c5a8481452c968c0a01811
Cong-Xin Qiu <ozoox.o@gmail.com>
2017-07-22 16:18:18 +0800

    Using Tiles 3 template to link to React

--- capsid/src/main/java/enterovirus/capsid/database/MemberRepository.java
+++ capsid/src/main/java/enterovirus/capsid/database/MemberRepository.java
@@ -2,7 +2,6 @@ package enterovirus.capsid.database;
 
 import java.util.List;
 import org.springframework.data.repository.PagingAndSortingRepository;
-import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import enterovirus.capsid.domain.MemberBean;
@@ -10,7 +9,7 @@ import enterovirus.capsid.domain.MemberBean;
 @RepositoryRestResource(collectionResourceRel="users", path="users")
 public interface MemberRepository extends PagingAndSortingRepository<MemberBean, Integer> {
 
-	List<MemberBean> findByUsername(@Param("username") String username);
+	List<MemberBean> findByUsername(String username);
 	
 	/* In Spring Data JPA, save() does both jobs of INSERT and UPDATE,
 	 * depend on whether the primary key is the same or not. 
--- capsid/src/main/java/enterovirus/capsid/database/OrganizationRepository.java
+++ capsid/src/main/java/enterovirus/capsid/database/OrganizationRepository.java
@@ -2,7 +2,6 @@ package enterovirus.capsid.database;
 
 import java.util.List;
 import org.springframework.data.repository.PagingAndSortingRepository;
-import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import enterovirus.capsid.domain.OrganizationBean;
@@ -15,6 +14,6 @@ import enterovirus.capsid.domain.OrganizationBean;
 @RepositoryRestResource(collectionResourceRel="organizations", path="organizations")
 public interface OrganizationRepository extends PagingAndSortingRepository<OrganizationBean, Integer> {
 
-	List<OrganizationBean> findByName(@Param("name") String name);
+	List<OrganizationBean> findByName(String name);
 	OrganizationBean saveAndFlush(OrganizationBean member);
 }
