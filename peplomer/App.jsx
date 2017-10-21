83dad5931340ea5bc9839ff9d8ce7fa8b5407ed2
C Qiu <ozoox.o@gmail.com>
2017-10-21 08:20:23 -0400

    Change App.jsx tab

--- peplomer/App.jsx
+++ peplomer/App.jsx
@@ -1,67 +1,73 @@
 import React from 'react';
 
 class App extends React.Component {
-   constructor() {
-      super();
-		
-      this.state = {
-         data: 
-         [
-            {
-               "id":1,
-               "name":"Foo",
-               "age":"20"
-            },
-				
-            {
-               "id":2,
-               "name":"Bar",
-               "age":"30"
-            },
-				
-            {
-               "id":3,
-               "name":"Baz",
-               "age":"40"
-            }
-         ]
-      }
-   }
-	
-   render() {
-      return (
-         <div>
-            <Header/>
-            <table>
-               <tbody>
-                  {this.state.data.map((dummy, i) => <TableRow key = {i} data = {dummy} />)}
-               </tbody>
-            </table>
-         </div>
-      );
-   }
+	constructor() {
+		super();
+
+		this.state = {
+			data:
+			[
+				{
+					"id":1,
+					"name":"Foo",
+					"age":"20"
+				},
+
+				{
+					"id":2,
+					"name":"Bar",
+					"age":"30"
+				},
+
+				{
+					"id":3,
+					"name":"Baz",
+					"age":"40"
+				}
+			]
+		}
+	}
+
+	componentDidMount() {
+		client({method: 'GET', path: '/api/employees'}).done(response => {
+			this.setState({employees: response.entity._embedded.employees});
+		});
+	}
+
+	render() {
+		return (
+			<div>
+				<Header/>
+				<table>
+					<tbody>
+						{this.state.data.map((dummy, i) => <TableRow key = {i} data = {dummy} />)}
+					</tbody>
+				</table>
+			</div>
+		);
+	}
 }
 
 class Header extends React.Component {
-   render() {
-      return (
-         <div>
-            <h1>Header</h1>
-         </div>
-      );
-   }
+	render() {
+		return (
+			<div>
+				<h1>Header</h1>
+			</div>
+		);
+	}
 }
 
 class TableRow extends React.Component {
-   render() {
-      return (
-         <tr>
-            <td>{this.props.data.id}</td>
-            <td>{this.props.data.name}</td>
-            <td>{this.props.data.age}</td>
-         </tr>
-      );
-   }
+	render() {
+		return (
+			<tr>
+				<td>{this.props.data.id}</td>
+				<td>{this.props.data.name}</td>
+				<td>{this.props.data.age}</td>
+			</tr>
+		);
+	}
 }
 
 export default App;
