6a1fe8833e8044e87fbd7728579ca60f07dd03df
C Qiu <ozoox.o@gmail.com>
2017-12-19 17:02:35 -0500

    gihook update call gitar. Not work yet.

--- gihook/update/src/main/java/enterovirus/gihook/update/App.java
+++ gihook/update/src/main/java/enterovirus/gihook/update/App.java
@@ -1,8 +1,14 @@
 package enterovirus.gihook.update;
 
+import java.io.File;
+import java.io.IOException;
+
+import enterovirus.gitar.GitCommit;
+import enterovirus.gitar.wrap.CommitSha;
+
 public class App {
 	
-	public static void main (String[] args) {
+	public static void main (String[] args) throws IOException {
 		
 		String branchName = args[0];
 		String oldCommitSha = args[1];
@@ -11,5 +17,12 @@ public class App {
 		System.out.println("branchName: "+branchName);
 		System.out.println("oldCommitSha: "+oldCommitSha);
 		System.out.println("newCommitSha: "+newCommitSha);
+		
+		System.out.println("Current directory: "+System.getProperty("user.dir"));
+		
+		File repositoryDirectory = new File(System.getProperty("user.dir"));
+		CommitSha commitSha = new CommitSha(newCommitSha);
+		
+		GitCommit commit = new GitCommit(repositoryDirectory, commitSha);
 	}
 }
