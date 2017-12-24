f1cf1f636b6a185aafbc5534d1fbe3ffaaffefa3
C Qiu <ozoox.o@gmail.com>
2017-12-24 17:12:29 -0500

    Try to downgrade proteinsistence from a Spring Boot package to a Spring package. RepositoryRepositoryTest works.

---
+++ proteinsistence/src/test/java/enterovirus/proteinsistence/ComponentScanConfig.java
@@ -0,0 +1,11 @@
+package enterovirus.proteinsistence;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan
+public class ComponentScanConfig {
+
+}
+
--- proteinsistence/src/test/java/enterovirus/proteinsistence/database/RepositoryRepositoryTest.java
+++ proteinsistence/src/test/java/enterovirus/proteinsistence/database/RepositoryRepositoryTest.java
@@ -5,13 +5,14 @@ import java.io.IOException;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import enterovirus.proteinsistence.ComponentScanConfig;
 import enterovirus.proteinsistence.domain.RepositoryBean;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes=ComponentScanConfig.class)
 public class RepositoryRepositoryTest {
 
 	@Autowired RepositoryRepository repositoryRepository;
