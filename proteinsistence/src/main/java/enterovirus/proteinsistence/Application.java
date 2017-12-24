badca7d9908db5ce4eafd7f6ec9fd58491a409f3
C Qiu <ozoox.o@gmail.com>
2017-12-24 17:43:43 -0500

    Package gihook/update, unit test works.

--- proteinsistence/src/main/java/enterovirus/proteinsistence/Application.java
+++ proteinsistence/src/main/java/enterovirus/proteinsistence/Application.java
@@ -6,14 +6,32 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication
+@ComponentScan(basePackages = {"enterovirus.proteinsistence"})
 public class Application {
-
-	private static final Logger log = LoggerFactory.getLogger(Application.class);
-
+	
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class);
+		ApplicationContext context = new AnnotationConfigApplicationContext(Application.class);
+		Application p = context.getBean(Application.class);
+		p.run();
 	}
+	
+	private void run () {
+		
+	}
+	
 }
+
+//@SpringBootApplication
+//public class Application {
+//
+//	private static final Logger log = LoggerFactory.getLogger(Application.class);
+//
+//	public static void main(String[] args) {
+//		SpringApplication.run(Application.class);
+//	}
+//}
