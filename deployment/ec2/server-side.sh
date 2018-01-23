d53cf1739afedd9f0113430a0a1e771cf64f62ca
C Qiu <ozoox.o@gmail.com>
2018-01-23 12:29:46 -0500

    Port 80 works.

--- deployment/ec2/server-side.sh
+++ deployment/ec2/server-side.sh
@@ -1,3 +1,5 @@
+sudo apt-get install git # It seems already installed by default.
+
 sudo apt-get install postgresql postgresql-contrib
 sudo -u postgres psql postgres
 \password postgres
@@ -9,5 +11,8 @@ sudo apt-get install tomcat8
 sudo chmod 777 /var/lib/tomcat8/webapps
 rm -rf /var/lib/tomcat8/webapps/ROOT
 
-#sudo sed -i "s/    <Connector port=\"8080\" protocol=\"HTTP\/1.1\"/    <Connector port=\"80\" protocol=\"HTTP\/1.1\"/g" /var/lib/tomcat8/conf/server.xml
-#sudo service tomcat8 restart
+sudo /sbin/iptables -t nat -I PREROUTING -p tcp --dport 80 -j REDIRECT --to-port 8080
+sudo apt-get install iptables-persistent
+sudo netfilter-persistent save
+sudo netfilter-persistent reload
+sudo sed -i "s/    <Connector port=\"8080\" protocol=\"HTTP\/1.1\"/    <Connector port=\"8080\" proxyPort=\"80\" protocol=\"HTTP\/1.1\"/g" /var/lib/tomcat8/conf/server.xml
