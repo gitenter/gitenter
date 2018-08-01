2352743dcc8156b8a689052c1792dc25d576b1bc
Cong-Xin Qiu <ozoox.o@gmail.com>
2018-08-01 18:58:08 -0400

    Redirect tomcat to port 80

--- chef-repo/cookbooks/tomcat/recipes/default.rb
+++ chef-repo/cookbooks/tomcat/recipes/default.rb
@@ -13,3 +13,28 @@ end
 apt_package 'tomcat8' do
   action :install
 end
+
+apt_package 'iptables-persistent' do
+  action :install
+end
+
+include_recipe 'iptables::default'
+
+iptables_rule 'http_8080' do
+  lines '-I PREROUTING -p tcp --dport 80 -j REDIRECT --to-port 8080'
+  table :nat
+end
+
+# Not idempotent but no longer needed
+# execute "netfilter-persistent save"
+# execute "netfilter-persistent reload"
+
+node.default['tomcat']['proxyPort'] = "80"
+
+template '/var/lib/tomcat8/conf/server.xml' do
+  source 'server.xml.erb'
+end
+
+service "tomcat8" do
+  action :restart
+end
