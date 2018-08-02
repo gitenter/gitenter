bc60391228e9c783591e8f1ff85058dbc0bd405e
Cong-Xin Qiu <ozoox.o@gmail.com>
2018-08-02 07:04:09 -0400

    Tiny remodulization

--- chef-repo/cookbooks/tomcat/recipes/default.rb
+++ chef-repo/cookbooks/tomcat/recipes/default.rb
@@ -14,26 +14,7 @@ apt_package 'tomcat8' do
   action :install
 end
 
-apt_package 'iptables-persistent' do
-  action :install
-end
-
-include_recipe 'iptables::default'
-
-iptables_rule 'http_8080' do
-  lines '-I PREROUTING -p tcp --dport 80 -j REDIRECT --to-port 8080'
-  table :nat
-end
-
-# Not idempotent but no longer needed
-# execute "netfilter-persistent save"
-# execute "netfilter-persistent reload"
-
-node.default['tomcat']['proxyPort'] = "80"
-
-template '/var/lib/tomcat8/conf/server.xml' do
-  source 'server.xml.erb'
-end
+include_recipe 'tomcat::port_redirect'
 
 service "tomcat8" do
   action :restart
