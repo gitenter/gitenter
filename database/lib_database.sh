77a19192aee90843c1c0d2470f7d878b756b8065
Cong-Xin Qiu <ozoox.o@gmail.com>
2019-03-12 19:00:36 -0400

    Alter sequence to bypass dbUnit bug

--- database/lib_database.sh
+++ database/lib_database.sh
@@ -17,4 +17,5 @@ init_database () {
     export PGHOST=localhost
     psql -U gitenter -d $dbname -w -f initiate_database.sql
     psql -U gitenter -d $dbname -w -f privilege_control.sql
+    psql -U gitenter -d $dbname -w -f alter_sequence.sql
 }
