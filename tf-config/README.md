364479e80c2051f446a299866989c198d0032444
Cong-Xin Qiu <ozoox.o@gmail.com>
2019-03-26 18:49:34 -0400

    Add multiple ECS needed roles

--- tf-config/README.md
+++ tf-config/README.md
@@ -1,5 +1,5 @@
 # Terraform
 
-AWS console sign in page: https://gitenter.signin.aws.amazon.com/console https://662490392829.signin.aws.amazon.com/console
+AWS console sign in page: https://gitenter.signin.aws.amazon.com/console
 
 Manually created a user `Administrator` with permissions `AdministratorAccess`. The `access_key` and `secret_key` of this user is used to setup users in `/iam`.
--- tf-config/iam/main.tf
+++ tf-config/iam/main.tf
@@ -11,21 +11,59 @@ provider "aws" {
   region = "us-east-1"
 }
 
-data "aws_iam_policy" "AmazonEC2FullAccess" {
-  arn = "arn:aws:iam::aws:policy/AmazonEC2FullAccess"
-}
-
 resource "aws_iam_group" "terraform" {
   name = "terraform"
 }
 
-resource "aws_iam_group_policy_attachment" "terraform-ec2_attach" {
-  group = "${aws_iam_group.terraform.id}"
-  policy_arn = "${data.aws_iam_policy.AmazonEC2FullAccess.arn}"
-}
-
+# TODO:
+# Create a series of users from list variable. Straightforward, can follow e.g.
+# https://medium.com/devopslinks/aws-iam-user-and-policy-creation-using-terraform-7cd781e06c97
+# However, it seems not that easy to create `aws_iam_access_key` for that
+# series: `aws_iam_access_key.terraform: user must be a single value, not a list`
 resource "aws_iam_user" "terraform" {
   name = "terraform"
+}
+
+data "aws_iam_policy" "terraform-ec2" {
+  arn = "arn:aws:iam::aws:policy/AmazonEC2FullAccess"
+}
+
+resource "aws_iam_group_policy_attachment" "terraform-ec2" {
+  group = "${aws_iam_group.terraform.id}"
+  policy_arn = "${data.aws_iam_policy.terraform-ec2.arn}"
+}
+
+# Multiple roles are added for ECS propose:
+# - `AmazonEC2ContainerServiceforEC2Role`
+# - `AmazonEC2ContainerServiceRole`
+# - `AmazonECSTaskExecutionRolePolicy`
+# https://docs.aws.amazon.com/AmazonECS/latest/developerguide/get-set-up-for-amazon-ecs.html
+data "aws_iam_policy" "terraform-ecs_container_instance" {
+  arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2ContainerServiceforEC2Role"
+}
+
+resource "aws_iam_group_policy_attachment" "terraform-ecs_container_instance" {
+  group = "${aws_iam_group.terraform.id}"
+  policy_arn = "${data.aws_iam_policy.terraform-ecs_container_instance.arn}"
+}
+
+# May be fully covered by `AmazonEC2FullAccess` but still add it in here.
+data "aws_iam_policy" "terraform-ecs" {
+  arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2ContainerServiceRole"
+}
+
+resource "aws_iam_group_policy_attachment" "terraform-ecs" {
+  group = "${aws_iam_group.terraform.id}"
+  policy_arn = "${data.aws_iam_policy.terraform-ecs.arn}"
+}
+
+data "aws_iam_policy" "terraform-ecs_fargate" {
+  arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
+resource "aws_iam_group_policy_attachment" "terraform-ecs_fargate" {
+  group = "${aws_iam_group.terraform.id}"
+  policy_arn = "${data.aws_iam_policy.terraform-ecs_fargate.arn}"
 }
 
 resource "aws_iam_access_key" "terraform" {
