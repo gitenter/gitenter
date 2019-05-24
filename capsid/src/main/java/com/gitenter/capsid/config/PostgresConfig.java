0e3ba2f525e3616a75030ae67d2af8b29f49fb26
Cong-Xin Qiu <ozoox.o@gmail.com>
2019-05-24 07:27:14 -0400

    Setup Spring session on STS

--- capsid/src/main/java/com/gitenter/capsid/config/PostgresConfig.java
+++ capsid/src/main/java/com/gitenter/capsid/config/PostgresConfig.java
@@ -8,7 +8,7 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
 @Configuration
-public class DatabaseConfig {
+public class PostgresConfig {
 
 	@Profile("sts")
 	@Bean
---
+++ capsid/src/main/java/com/gitenter/capsid/config/RedisConfig.java
@@ -0,0 +1,44 @@
+package com.gitenter.capsid.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+
+@Configuration
+public class RedisConfig {
+
+	@Profile("sts")
+	@Bean
+	public RedisConnectionFactory stsConfiguration() {
+		RedisStandaloneConfiguration configuration = new RedisStandaloneConfiguration("localhost", 6379);
+		return new JedisConnectionFactory(configuration);
+	}
+
+	@Profile("docker")
+	@Bean
+	public RedisConnectionFactory dockerConfiguration() {
+		RedisStandaloneConfiguration configuration = new RedisStandaloneConfiguration("localhost", 6379);
+		return new JedisConnectionFactory(configuration);
+	}
+
+	@Profile("qa")
+	@Bean
+	public RedisConnectionFactory qaConfiguration() {
+		RedisStandaloneConfiguration configuration = new RedisStandaloneConfiguration("localhost", 6379);
+		return new JedisConnectionFactory(configuration);
+	}
+
+	@Profile("production")
+	@Bean
+	public RedisConnectionFactory productionConfiguration() {
+		/*
+		 * TODO:
+		 * Setup password.
+		 */
+		RedisStandaloneConfiguration configuration = new RedisStandaloneConfiguration("localhost", 6379);
+		return new JedisConnectionFactory(configuration);
+	}
+}
