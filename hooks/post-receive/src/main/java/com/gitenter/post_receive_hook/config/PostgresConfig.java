4bcf1ac4acc6c6642f7b9ace1f64ca9affef0bd7
Cong-Xin Qiu <ozoox.o@gmail.com>
2019-07-31 07:46:11 -0400

    Enable STS profile
    We cannot have a mock profile which returns mock(DataSource.class), because then Hibernate/Jpa will raise a
    NullPointerException basically saying it cannot build a proper connection to the database.
    > [PersistenceUnit: default] Unable to build Hibernate SessionFactory; nested exception is java.lang.NullPointerException
    
    The relevant tests are properly mocked so they don't need the database at all. However, as we are inside
    of the framework we still need an environment with proper database to make tests pass.

--- hooks/post-receive/src/main/java/com/gitenter/post_receive_hook/config/PostgresConfig.java
+++ hooks/post-receive/src/main/java/com/gitenter/post_receive_hook/config/PostgresConfig.java
@@ -4,13 +4,15 @@ import javax.sql.DataSource;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
 @Configuration
 public class PostgresConfig {
 
+	@Profile("sts")
 	@Bean
-	public DataSource dataSource() {
+	public DataSource stsDataSource() {
 		DriverManagerDataSource dataSource = new DriverManagerDataSource();
 		dataSource.setDriverClassName("org.postgresql.Driver");
 		dataSource.setUrl("jdbc:postgresql://localhost:5432/gitenter");
