003c8d4c664446a09633ed9e06192e9542f1265a
Cong-Xin Qiu <ozoox.o@gmail.com>
2019-07-31 07:54:11 -0400

    Setup missing profiles

--- hooks/post-receive/src/main/java/com/gitenter/post_receive_hook/config/PostgresConfig.java
+++ hooks/post-receive/src/main/java/com/gitenter/post_receive_hook/config/PostgresConfig.java
@@ -20,4 +20,37 @@ public class PostgresConfig {
 		dataSource.setPassword("zooo");
 		return dataSource;
 	}
+	
+	@Profile("docker")
+	@Bean
+	public DataSource dockerDataSource() {
+		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+		dataSource.setDriverClassName("org.postgresql.Driver");
+		dataSource.setUrl("jdbc:postgresql://postgres:5432/gitenter");
+		dataSource.setUsername("gitenter_app");
+		dataSource.setPassword("zooo");
+		return dataSource;
+	}
+
+	@Profile("staging")
+	@Bean
+	public DataSource stagingDataSource() {
+		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+		dataSource.setDriverClassName("org.postgresql.Driver");
+		dataSource.setUrl("jdbc:postgresql://staging-postgres.cqx7dy9nh94t.us-east-1.rds.amazonaws.com:5432/gitenter");
+		dataSource.setUsername("gitenter_app");
+		dataSource.setPassword("zooo");
+		return dataSource;
+	}
+
+	@Profile("production")
+	@Bean
+	public DataSource productionDataSource() {
+		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+		dataSource.setDriverClassName("org.postgresql.Driver");
+		dataSource.setUrl("jdbc:postgresql://localhost:5432/gitenter");
+		dataSource.setUsername("gitenter_app");
+		dataSource.setPassword("zooo");
+		return dataSource;
+	}
 }
