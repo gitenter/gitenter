b884db1f5959971fc25bcd3739ddfb5f78563142
Cong-Xin Qiu <ozoox.o@gmail.com>
2019-08-16 08:11:41 -0400

    SSH environment variable. Recorded m utliple ways. Still not working for our case.

--- ssheep/entrypoint.sh
+++ ssheep/entrypoint.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
-echo "export SPRING_PROFILES_ACTIVE=$SPRING_PROFILES_ACTIVE" >> /etc/profile
+# echo "export SPRING_PROFILES_ACTIVE=$SPRING_PROFILES_ACTIVE" >> /etc/profile
+
+# echo "Host *
+#   SetEnv SPRING_PROFILES_ACTIVE=$SPRING_PROFILES_ACTIVE" >> /home/git/.ssh/config
+
+echo "SPRING_PROFILES_ACTIVE=$SPRING_PROFILES_ACTIVE" >> /home/git/.ssh/environment
+
 exec "$@"
