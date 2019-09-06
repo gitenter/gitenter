cec7ff1be9bcf94efba70b098d08e1c100ab759d
Cong-Xin Qiu <ozoox.o@gmail.com>
2019-09-06 18:58:46 -0400

    Use GitPython instead of pygit2. Locally python3 -m unittest tests.repository_navigation_test works

--- selenium-test/settings/profile.py
+++ selenium-test/settings/profile.py
@@ -49,15 +49,11 @@ class Profile(object):
             remote_git_path = self.git_server_remote_location / org_name / "{}.git".format(repo_name)
             return "file://{}".format(str(remote_git_path))
         elif type(self.git_server_remote_location) == str:
-            # Note that `pygit2.clone_repository` needs URL in a very special format.
-            # pygit2:    git://github.com/libgit2/pygit2.git
-            # git clone: git@github.com:libgit2/pygit2.git
-            #            ssh://git@github.com:[port]/libgit2/pygit2.git
-            # with a customized port, `git clone` only works for `ssh:` format,
-            # while `pygit2.clone_repository` doesn't support that, with error
-            # > _pygit2.GitError: invalid hex digit in length: 'SSH-'
-            return "git://{}/home/git/{}/{}.git".format(
+            # May use the SSH protocol one if we need a customized port.
+            return "git@{}:{}/{}.git".format(
                 self.git_server_remote_location, org_name, repo_name)
+            # return "ssh://git@{}:22/{}/{}.git".format(
+            #     self.git_server_remote_location, org_name, repo_name)
 
 
 class LocalProfile(Profile):
