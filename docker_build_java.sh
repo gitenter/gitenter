6556c7c4b894835ec420ed424dd56e2010a44b5e
Cong-Xin Qiu <ozoox.o@gmail.com>
2019-09-18 19:01:20 -0400

    CircleCI workflow change

--- docker_build_java.sh
+++ docker_build_java.sh
@@ -1,3 +1,9 @@
+# jar/war build by a different profile are identical. However, those changes
+# are necessary to make unit test passing in docker build.
+#
+# TODO:
+# Is it possible to pass in those profiles in docker env variable, so we don't
+# need to `sed` in here?
 sed -i'.original' -e "s/\t\tdataSource.setUrl(\"jdbc:postgresql:\/\/localhost:5432\/gitenter\");/\t\tdataSource.setUrl(\"jdbc:postgresql:\/\/postgres:5432\/gitenter\");/g" protease/src/test/java/com/gitenter/protease/config/TestPostgresConfig.java
 sed -i'.original' -e "s/@ActiveProfiles(\"local\")/@ActiveProfiles(\"docker\")/g" hooks/post-receive/src/test/java/com/gitenter/post_receive_hook/service/UpdateDatabaseFromGitServiceTest.java
 
