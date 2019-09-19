36000027ab1e4b62b8831752c0fa334a4146767f
Cong-Xin Qiu <ozoox.o@gmail.com>
2019-09-19 07:36:59 -0400

    Remove dev-machine container

--- docker_build_java.sh
+++ docker_build_java.sh
@@ -4,6 +4,14 @@
 # TODO:
 # Is it possible to pass in those profiles in docker env variable, so we don't
 # need to `sed` in here?
+#
+# Note: we can in genral setup netowrk atlas in docker-compose, but `localhost`
+# doesn't work.
+# >  links:
+# >    - "database:localhost"
+# Also, in `protease` profiles are used for different git repo (e.g. `minimal`)
+# and that's the reason it is not easy to setup profiles like local/docker/...
+# like other package.
 sed -i'.original' -e "s/\t\tdataSource.setUrl(\"jdbc:postgresql:\/\/localhost:5432\/gitenter\");/\t\tdataSource.setUrl(\"jdbc:postgresql:\/\/postgres:5432\/gitenter\");/g" protease/src/test/java/com/gitenter/protease/config/TestPostgresConfig.java
 sed -i'.original' -e "s/@ActiveProfiles(\"local\")/@ActiveProfiles(\"docker\")/g" hooks/post-receive/src/test/java/com/gitenter/post_receive_hook/service/UpdateDatabaseFromGitServiceTest.java
 
