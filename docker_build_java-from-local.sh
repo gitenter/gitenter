bbbdb714485ccf344807ba8d9a47e51952ef815b
Cong-Xin Qiu <ozoox.o@gmail.com>
2019-09-20 18:33:28 -0400

    Cleanup

--- docker_build_java-from-local.sh
+++ docker_build_java-from-local.sh
@@ -3,8 +3,6 @@
 sed -i'.original' -e "s/spring.profiles.active=local/spring.profiles.active=docker/g" capsid/src/main/resources/application.properties
 sed -i'.original' -e "s/		System.setProperty(\"spring.profiles.active\", \"local\");/		System.setProperty(\"spring.profiles.active\", \"docker\");/g" hooks/post-receive/src/main/java/com/gitenter/post_receive_hook/PostReceiveApplication.java
 
-# TODO:
-# Consider moving this part to `java-build` in docker-compose definition.
 mvn clean install
 mvn compile assembly:single -f hooks/post-receive/pom.xml -DskipTests
 mvn package -f capsid/pom.xml -DskipTests
