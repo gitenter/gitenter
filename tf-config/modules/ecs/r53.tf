4ea4ccdaedc40a371082c5f619335b18f11e2621
Cong-Xin Qiu <ozoox.o@gmail.com>
2019-10-14 08:07:23 -0400

    r53 alias record. Confirmed working by ping staging-ecs-web-all-270510683.us-east-1.elb.amazonaws.com or staging.gitenter.com

--- tf-config/modules/ecs/r53.tf
+++ tf-config/modules/ecs/r53.tf
@@ -10,6 +10,28 @@ resource "aws_route53_record" "main" {
   name    = "${var.environment}.${data.aws_route53_zone.main.name}"
 
   type    = "A"
-  ttl     = "300"
-  records = ["10.0.0.1"]
+
+  # TODO:
+  # Ideally we want it to redirect to ALB (web) for port 80, and NLB (git) to
+  # port 22.
+  #
+  # Route53 doesn't support it, because DNS record just doesn't support port.
+  #
+  # One possibility is to have a NLB (all) in front of everything, and it
+  # redirect to the web ALB and git NLB based on port. (The very front one need
+  # to be a NLB, because ALB only supports 80 and 8080.)
+  # The setup is messy because forwarding needs fixed IP while the ALB IP is
+  # changing. Workaround:
+  # https://stackoverflow.com/questions/50981864/aws-pass-traffic-from-nlb-to-an-alb
+  # https://aws.amazon.com/blogs/networking-and-content-delivery/using-static-ip-addresses-for-application-load-balancers/
+  #
+  # Or we can have a Nginx in frout of them (one more kind of container + single
+  # point failure).
+  #
+  # Or we just need a different subdomain for git access (e.g. `staging.git.gitenter.com`)
+  alias {
+    name                   = "${aws_lb.web.dns_name}"
+    zone_id                = "${aws_lb.web.zone_id}"
+    evaluate_target_health = false
+  }
 }
