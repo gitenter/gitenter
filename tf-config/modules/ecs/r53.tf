d3be042fc04b561504f1f8e952adc1c15b4015ea
Cong-Xin Qiu <ozoox.o@gmail.com>
2019-10-14 18:21:43 -0400

    Split web and git route53 records. Conditionally handle www. and staging. cases

--- tf-config/modules/ecs/r53.tf
+++ tf-config/modules/ecs/r53.tf
@@ -2,36 +2,48 @@ data "aws_route53_zone" "main" {
   name = "gitenter.com"
 }
 
-resource "aws_route53_record" "main" {
+resource "aws_route53_record" "web" {
   zone_id = "${data.aws_route53_zone.main.zone_id}"
-
-  # TODO:
-  # Use `www` if environment is production.
-  name    = "${var.environment}.${data.aws_route53_zone.main.name}"
-
   type    = "A"
 
-  # TODO:
-  # Ideally we want it to redirect to ALB (web) for port 80, and NLB (git) to
-  # port 22.
-  #
-  # Route53 doesn't support it, because DNS record just doesn't support port.
-  #
-  # One possibility is to have a NLB (all) in front of everything, and it
-  # redirect to the web ALB and git NLB based on port. (The very front one need
-  # to be a NLB, because ALB only supports 80 and 8080.)
-  # The setup is messy because forwarding needs fixed IP while the ALB IP is
-  # changing. Workaround:
-  # https://stackoverflow.com/questions/50981864/aws-pass-traffic-from-nlb-to-an-alb
-  # https://aws.amazon.com/blogs/networking-and-content-delivery/using-static-ip-addresses-for-application-load-balancers/
-  #
-  # Or we can have a Nginx in frout of them (one more kind of container + single
-  # point failure).
-  #
-  # Or we just need a different subdomain for git access (e.g. `staging.git.gitenter.com`)
+  name    = "${var.environment == "production" ? "www" : var.environment}.${data.aws_route53_zone.main.name}"
+
   alias {
     name                   = "${aws_lb.web.dns_name}"
     zone_id                = "${aws_lb.web.zone_id}"
+    evaluate_target_health = false
+  }
+}
+
+# TODO:
+# Ideally we want both web (port 80) and git (port 22) to use
+# `gitenter.com`/`staging.gitenter.com`. That basically need a master load
+# balancer on top of everything (and redirect traffic to `aws_lb.web` and
+# `aws_lb.git` accordingly). Reason is in DNS level we talked nothing about
+# ports.
+#
+# It is hard to be done using existing load balancers in AWS, as ALB have
+# better support (e.g. we need pattern matching forwarding) but it only supports
+# port 80 and 8080. NLB (currently used for git access for us) supports all ports,
+# but it only have limited functions. We may have a NLB on top of the current
+# `aws_lb.web` and `aws_lb.git`, but it is hard for an NLB to route to ALB
+# because the later one doesn't have a fixed IP. Workarounds:
+# https://stackoverflow.com/questions/50981864/aws-pass-traffic-from-nlb-to-an-alb
+# https://aws.amazon.com/blogs/networking-and-content-delivery/using-static-ip-addresses-for-application-load-balancers/
+#
+# Or we can have a Nginx in frout of them (one more kind of container + single
+# point failure).
+resource "aws_route53_record" "git" {
+  zone_id = "${data.aws_route53_zone.main.zone_id}"
+  type    = "A"
+
+  # TODO:
+  # A way to not `var.environment == "production"` two times?
+  name    = "git.${var.environment == "production" ? "" : var.environment}${var.environment == "production" ? "" : "."}${data.aws_route53_zone.main.name}"
+
+  alias {
+    name                   = "${aws_lb.git.dns_name}"
+    zone_id                = "${aws_lb.git.zone_id}"
     evaluate_target_health = false
   }
 }
