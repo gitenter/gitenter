a43004659cd72892c31fbc60869d6ee4db98c6ad
Cong-Xin Qiu <ozoox.o@gmail.com>
2019-11-04 07:40:33 -0500

    Nuxt layouts

---
+++ envuelope/layouts/auth.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <header>
+      <a href="/"><img class="logo" src="/resources/image/logo.png" alt="enterovirus" height="48" width="192"></a>
+
+      <form id="command" action="/logout" method="POST">
+        <input type="submit" id="logout" value="Log out" />
+        <div>
+          <input type="hidden" name="_csrf" value="ca8d34f0-e243-4440-a31e-eea4d56f9afb" />
+        </div>
+      </form>
+
+      <form action="/settings" method="GET">
+        <input type="submit" value="Settings" />
+      </form>
+
+      <form action="/help/" method="GET">
+        <input type="submit" value="Help" />
+      </form>
+
+      <div class="text">Logged in as asdf</div>
+    </header>
+
+    <article>
+      <nuxt />
+    </article>
+
+    <footer>
+      <a href="/about/">About</a> | <a href="/contact/">Contact</a><br>
+      Copyright &copy; 2017-2019 GitEnter
+    </footer>
+  </div>
+</template>
+
+<style></style>
---
+++ envuelope/layouts/unauth.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <header>
+      <a href="/"><img class="logo" src="/resources/image/logo.png" alt="enterovirus" height="48" width="192"></a>
+
+      <form action="/help/" method="GET">
+        <input type="submit" value="Help" />
+      </form>
+
+      <form action="/pricing/" method="GET">
+        <input type="submit" value="Pricing" />
+      </form>
+    </header>
+
+    <article>
+      <nuxt />
+    </article>
+
+    <footer>
+      <a href="/about/">About</a> | <a href="/contact/">Contact</a><br>
+      Copyright &copy; 2017-2019 GitEnter
+    </footer>
+  </div>
+</template>
+
+<style></style>
